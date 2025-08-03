cf55c9330968f4130333270b4893e4560f0632ec
Tharinda Pamindu <ict2022100@as.rjt.ac.lk>
2025-08-03 08:21:38 +0530

    Add Atbash Cipher

---
+++ TextSafer/AtbashCipher.cpp
@@ -0,0 +1,29 @@
+#include "AtbashCipher.h"
+
+String^ AtbashCipher::encrypt(String^ plainText)
+{
+	String^ cipherText = "";
+
+	for (int i = 0; i < plainText->Length; i++) {
+
+		wchar_t currentChar = plainText[i];
+
+		if (currentChar >= 'A' && currentChar <= 'Z')
+			cipherText += (wchar_t)(155 - currentChar);
+
+		else if (currentChar >= 'a' && currentChar <= 'z')
+			cipherText += (wchar_t)(219 - currentChar);
+
+		else
+			cipherText += currentChar;
+
+	}
+
+	return cipherText;
+
+}
+
+String^ AtbashCipher::decrypt(String^ cipherText)
+{
+	return encrypt(cipherText);
+}
